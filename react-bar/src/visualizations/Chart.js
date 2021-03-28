5e9506439647bb4959d84d01633d82b5e0292e5f
hyhong-code <hong961127@gmail.com>
2021-03-27 20:43:03 -0700

    Add bar chart with hooks

--- react-bar/src/visualizations/Chart.js
+++ react-bar/src/visualizations/Chart.js
@@ -1,25 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, Fragment, useEffect, useState } from "react";
 import * as d3 from "d3";
 
 const width = 650;
 const height = 400;
 const margin = { top: 20, right: 5, bottom: 20, left: 35 };
 
-class Chart extends Component {
-  state = {
-    bars: []
-  };
+const Chart = ({ data }) => {
+  const [bars, setBars] = useState([]);
 
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { data } = nextProps;
-    if (!data) return {};
+  useEffect(() => {
+    if (!data) return;
 
-    return {};
-  }
+    const dateExtent = d3.extent(data, (d) => d.date);
+    const xScale = d3.scaleTime().domain(dateExtent).range([0, width]);
 
-  render() {
-    return <svg width={width} height={height} />;
-  }
-}
+    const [tempMin, tempMax] = d3.extent(data, (d) => d.high);
+    const yScale = d3
+      .scaleLinear()
+      .domain([Math.min(tempMin, 0), tempMax])
+      .range([height, 0]);
+
+    const colorExtent = d3.extent(data, (d) => d.avg).reverse();
+    const colorScale = d3.scaleSequential().domain(colorExtent).interpolator(d3.interpolateRdYlBu);
+
+    setBars(
+      data.map((d) => ({
+        x: xScale(d.date),
+        y: yScale(d.high),
+        height: yScale(d.low) - yScale(d.high),
+        fill: colorScale(d.avg),
+      }))
+    );
+  }, [data]);
+
+  return (
+    <Fragment>
+      <svg width={width} height={height}>
+        {bars.map((bar, idx) => (
+          <rect key={idx} x={bar.x} y={bar.y} width="2" height={bar.height} fill={bar.fill} />
+        ))}
+      </svg>
+    </Fragment>
+  );
+};
 
 export default Chart;
